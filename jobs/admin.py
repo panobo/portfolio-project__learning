6a816175745f3a335f216b8a9e6a2a8eaa8ec9c2
Pavel Svejda <pavlis.1@seznam.cz>
2020-01-26 22:10:49 +0100

    Change of table name (Jobs -> Job)

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Jobs
+from .models import Job
 
-admin.site.register(Jobs)
+admin.site.register(Job)
 
--- jobs/models.py
+++ jobs/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Jobs(models.Model):
+class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
 
