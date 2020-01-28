711a9db6adfed7327a3c8598c9e52b33c47c1c1a
Pavel Svejda <pavlis.1@seznam.cz>
2020-01-28 17:35:50 +0100

    Added bootstrap and jobs are showing up.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,9 +18,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from . import views
+import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.job_home, name='job_home'),
+    path('', jobs.views.home, name='job_home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,2 @@
 from django.shortcuts import render
 
-
-def job_home(request):
-    return render(request, 'jobs/job_home.html')
